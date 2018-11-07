5660d6c6d889e07a832b99591542fe760afdb206
admin <vp@swifty.cloud>
2018-11-07 02:10:44 +0000

    minor fixes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const FACEBOOK_CLIENT_ID = 263933311081322
 export const FACEBOOK_URL = "https://localhost"
-export const AUTH_URL = "https://api.swifty.cloud/call/991d183ddd838c856cc7d6ed1289190647c08737ccff68881e8dc0209785f207"
-export const PROFILE_URL = "https://api.swifty.cloud/call/28109613ddcff22ad0ea9f4e61ed985f59973ef7df7f9c27f5df979829660108"
-export const PICTURE_URL = "https://api.swifty.cloud/call/e6a92e1fa8c10a5aa8bba2b0d47528e70c510598d960789f66a0e9b590dcc13d"
-export const TASKS_URL = "https://api.swifty.cloud/call/4b16b272df2e97dec3d1ab20770563126315a572a6ac1df9ae9e714ed707c6e7"
+export const AUTH_URL = "https://api.swifty.cloud/call/0894bcfe37df800092112d047d74f12162910a82aef228ba166c0d35cc88e728"
+export const PROFILE_URL = "https://api.swifty.cloud/call/b89edbf5032f6d9d81fc7dad078c85688df0623637d97f3c4259b05e953c57b1"
+export const PICTURE_URL = "https://api.swifty.cloud/call/7aee083506909e4d18daa146b2fd7a9b548669113d69d0f52cb52203f3c37551"
+export const TASKS_URL = "https://api.swifty.cloud/call/c10be9ddc38c873af0aa885d27e546418352b08564791a02a768ac0af3ba9de4"
